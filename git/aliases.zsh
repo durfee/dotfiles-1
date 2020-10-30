d2e6bf9f3b54bae60c64389f6c0084ecff3ff103
Sean <sean@house-house.design>
2020-10-30 10:45:30 -0700

    add my own aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,8 +14,10 @@ alias gp='git push origin HEAD'
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
+alias ga="git add"
 alias gc='git commit'
 alias gca='git commit -a'
+alias gcm="git commit -m "
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
---
+++ node/aliases.zsh
@@ -0,0 +1,8 @@
+alias ni="npm install"
+alias nid="npm install --save-dev"
+alias nig="npm install -g"
+alias nt="npm test"
+alias nit="npm install && npm test"
+alias nr="npm run"
+alias ns="npm start"
+alias nff="npm cache clean --force && rm -rf node_modules && npm install"
