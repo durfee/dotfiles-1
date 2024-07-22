d1ac47cb3d58c15eef9835f4da34a02be923c62f
durfee <durfee.sean@gmail.com>
2024-07-22 09:42:12 -0700

    idk just git up to date with a bunch of mostly broken stuff

--- pyenv/path.zsh
+++ pyenv/path.zsh
@@ -1,1 +1,3 @@
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+
+export PATH="/Users/durfee/.local/bin:$PATH"
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,8 +1,7 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
-then
+if $(gls &>/dev/null); then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
@@ -13,3 +12,5 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias -- -="cd -"
+
+alias c="cd $PROJECTS && la"
